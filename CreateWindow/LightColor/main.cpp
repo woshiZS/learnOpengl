bbf1d04dcf425a797da7a9540611ad0cc04e7345
woshiZS <jasonzhansen@foxmail.com>
2023-12-18 00:56:09 +0800

    finished light source section

--- CreateWindow/LightColor/main.cpp
+++ CreateWindow/LightColor/main.cpp
@@ -102,6 +102,19 @@ int main() {
 		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f
 	};
 
+	glm::vec3 cubePositions[] = {
+	glm::vec3(0.0f,  0.0f,  0.0f),
+	glm::vec3(2.0f,  5.0f, -15.0f),
+	glm::vec3(-1.5f, -2.2f, -2.5f),
+	glm::vec3(-3.8f, -2.0f, -12.3f),
+	glm::vec3(2.4f, -0.4f, -3.5f),
+	glm::vec3(-1.7f,  3.0f, -7.5f),
+	glm::vec3(1.3f, -2.0f, -2.5f),
+	glm::vec3(1.5f,  2.0f, -2.5f),
+	glm::vec3(1.5f,  0.2f, -1.5f),
+	glm::vec3(-1.3f,  1.0f, -1.5f)
+	};
+
 	unsigned int VAO, lightVAO;
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
@@ -135,13 +148,9 @@ int main() {
 	glEnableVertexAttribArray(6);
 
 	cubeShader->use();
-	glm::mat4 model(1.0f);
-	model = glm::translate(model, glm::vec3(0, 0, -3.0f));
-	model = glm::rotate(model, glm::radians(45.0f), glm::vec3(0.0f, 1.0f, 1.0f));
-	cubeShader->setMat4("model", glm::value_ptr(model));
 	
 	cubeShader->setVec3("material.specular", 0.5f, 0.5f, 0.5f);
-	cubeShader->setFloat("material.shininess", 64.0f);
+	cubeShader->setFloat("material.shininess", 16.0f);
 	cubeShader->setInt("material.diffuse", 0);
 	cubeShader->setInt("material.specular", 1);
 	cubeShader->setInt("material.emission", 2);
@@ -190,8 +199,15 @@ int main() {
 		cubeShader->setMat4("view", glm::value_ptr(view));
 		cubeShader->setMat4("projection", glm::value_ptr(projection));
 		cubeShader->setVec3("viewPos", glm::value_ptr(camera.GetCameraPos()));
-		cubeShader->setVec3("light.position", glm::value_ptr(lightPos));
+		cubeShader->setFloat("light.constant", 1.0f);
+		cubeShader->setFloat("light.linear", 0.09f);
+		cubeShader->setFloat("light.quadratic", 0.032f);
 
+		cubeShader->setVec3("light.position", glm::value_ptr(camera.GetCameraPos()));
+		cubeShader->setVec3("light.direction", glm::value_ptr(camera.GetCameraFront()));
+		cubeShader->setFloat("light.cutOff", glm::cos(glm::radians(12.5f)));
+		cubeShader->setFloat("light.outerCutOff", glm::cos(glm::radians(17.5f)));
+		//cubeShader->setVec3("light.direction", -0.2f, -1.0f, -0.3f);
 
 		cubeShader->setVec3("light.ambient", glm::value_ptr(ambientColor));
 		cubeShader->setVec3("light.diffuse", glm::value_ptr(diffuseColor));
@@ -203,7 +219,14 @@ int main() {
 
 		glBindVertexArray(VAO);
 		cubeShader->use();
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+		for (unsigned int i = 0; i < 10; ++i) {
+			glm::mat4 model(1.0f);
+			model = glm::translate(model, cubePositions[i]);
+			float angle = 20.0f * i;
+			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+			cubeShader->setMat4("model", glm::value_ptr(model));
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
 
 		glBindVertexArray(lightVAO);
 		lightShader->use();
