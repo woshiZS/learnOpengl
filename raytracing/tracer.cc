7c02ee20de251b8783fe8c3a95690f2c62e07d1b
woshiZS <jasonzhansen@foxmail.com>
2024-01-14 18:09:25 +0800

    finished hittable objects and normal part

--- raytracing/tracer.cc
+++ raytracing/tracer.cc
@@ -1,26 +1,20 @@
-#include <iostream>
+#include "rtweekend.h"
 
 #include "color.h"
-#include "ray.h"
-#include "vec3.h"
+#include "hittable.h"
+#include "hittable_list.h"
+#include "sphere.h"
 
-bool hit_sphere(const point3& center, double radius, const ray& r)
-{
-    // can cache variable
-    vec3 oc = r.origin() - center;
-    auto a = dot(r.direction(), r.direction());
-    auto b = 2 * dot(r.direction(), oc);
-    auto c = dot(oc, oc) - radius * radius;
+#include <iostream>
 
-    return (b * b - 4 * a * c) >= 0;
-}
-
-color ray_color(const ray& r)
+color ray_color(const ray& r, const hittable& world)
 {
     // hard coded sphre 现在的光线还无法区分方向，只要有解，都会显示红色小球。
-    if(hit_sphere(point3(0, 0, -1), 0.5, r))
-        return color(1, 0, 0);
-
+    hit_record rec;
+    if(world.hit(r, interval(0, infinity), rec))
+    {
+        return 0.5 * (rec.normal + 1);
+    }
 
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0); // scale the range to [0, 1]
@@ -37,6 +31,14 @@ int main()
     // Calculate the image height, and ensure that it's at least 1.
     int image_height = static_cast<int>(image_width / aspect_ratio);
     image_height = (image_height < 1) ? 1 : image_height;
+
+    // World
+
+    hittable_list world;
+
+    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+
 
     // Camera
 
@@ -73,7 +75,7 @@ int main()
             // direction is not a unit vector here.
             ray r(camera_center, ray_direction);
 
-            color pixel_color = ray_color(r);
+            color pixel_color = ray_color(r, world);
             write_color(std::cout, pixel_color);
         }
     }
